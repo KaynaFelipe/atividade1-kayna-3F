79c35fe43bb96591f60a6da74479f2b5208bc898
Kaynã Felipe <110180067+KaynaFelipe@users.noreply.github.com>
2024-08-13 21:57:24 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const meuNome = "Kayna";
+const idade = 17;
+const altura = 1.73;
+const peso = 50;
+const cidade = "São José dos Pinhais";
+console.log(meuNome);
+console.log(idade);
+console.log(altura);
+console.log(peso);
+console.log(cidade);
